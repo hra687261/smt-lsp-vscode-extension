632447430973e4ee332a19111a4ea8bd29b7a7c1
hra687261 <hra687261@gmail.com>
2024-05-09 23:11:59 +0200

    Added support for providing the path to the binary And some language configuration like comments and surrounding with parenthesis

--- extension.ts
+++ extension.ts
@@ -7,34 +7,49 @@ import {
   Executable,
   DidChangeConfigurationNotification
 } from 'vscode-languageclient/node';
+import * as path from 'path';
+import * as os from 'os';
 
 let client: LanguageClient;
 
+function get_binary(): string {
+  let bin: string =
+    workspace.getConfiguration("smt-lsp").get("binary") || "dolmenls"
+  bin =
+    bin.startsWith('~') ?
+      path.join(os.homedir(), bin.slice(1)) :
+      bin.startsWith('$HOME') ?
+        path.join(os.homedir(), bin.slice(5)) :
+        bin
+  return bin
+}
+
 export function activate(_context: ExtensionContext) {
 
-  const run: Executable = {
-    command: 'dolmenls'
-  };
+  let cmd = get_binary()
+  const run: Executable = { command: cmd }
 
   client = new LanguageClient(
-    'dolmenls',
+    cmd,
     'SMT Language Server Protocol',
     { run, debug: run },
-    { documentSelector: [{ scheme: 'file', language: 'smt' }] },
-  );
+    { documentSelector: [{ scheme: 'file', language: 'smt' }] }
+  )
 
   client.start();
-  client.sendNotification(
-    DidChangeConfigurationNotification.type,
-    { settings: workspace.getConfiguration("smt-lsp") }
-  );
+  let preludes: string[] = workspace.getConfiguration("smt-lsp").get("preludes", [])
+  if (preludes.length)
+    client.sendNotification(
+      DidChangeConfigurationNotification.type,
+      { settings: { "preludes": preludes } }
+    )
 
   workspace.onDidChangeConfiguration(e => {
     if (e.affectsConfiguration("smt-lsp.preludes")) {
       client.sendNotification(
         DidChangeConfigurationNotification.type,
         { settings: workspace.getConfiguration("smt-lsp") }
-      );
+      )
     }
   })
 }
